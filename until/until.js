90081cb9e87a170eb08cff29cdac2d0c994e874e
practice <460131185@qq.com>
2018-10-31 01:14:50 +0800

    完善注册验证功能

--- until/until.js
+++ until/until.js
@@ -3,7 +3,7 @@ const events = require('events').EventEmitter;
 const eventsEmitter = new events;
 const event = eventsEmitter;
 let _docData = ["articals", "users", "comments"],
-    _docLen = _docData.length;
+	_docLen = _docData.length;
 let _mongoData = '';
 
 /**
@@ -19,48 +19,55 @@ let _mongoData = '';
  * @param callback 回调函数；
  */
 myUntils.getMongoData = function (choice, callback) {
-    let _finishNum = 0,
-        _allData = [];
-    connectMongo(function () {
-        eventsEmitter.emit('finishOne')
-    });
-    //把数据库的内容传递回去；；
-    eventsEmitter.on('finishOne', function () {
-        _finishNum++;
-        if (_finishNum >= _docLen) {
-            eventsEmitter.emit('finishAll');
-        }
-    })
-    eventsEmitter.on('finishAll', function () {
-            finishPack();
-            _finishNum = 0;
-            eventsEmitter.removeAllListeners();
-            _mongoData = _choiceData(choice, _mongoData);
-            callback.call(null, _mongoData);
-        }
-    )
+	let _finishNum = 0,
+		_allData = [];
+	connectMongo(function () {
+		eventsEmitter.emit('finishOne')
+	});
+	//把数据库的内容传递回去；；
+	eventsEmitter.on('finishOne', function () {
+		_finishNum++;
+		if (_finishNum >= _docLen) {
+			eventsEmitter.emit('finishAll');
+		}
+	})
+	eventsEmitter.on('finishAll', function () {
+			finishPack();
+			_finishNum = 0;
+			eventsEmitter.removeAllListeners();
+			_mongoData = _choiceData(choice, _mongoData);
+			callback.call(null, _mongoData);
+		}
+	)
 }
 
 /**
  * 对值进行加密
  */
 myUntils.setCrypro = function (data) {
-    const crypro = require('crypto');
-    const KEY = 'hejiancheng hen li hai'
-    const hmac = crypro.createHmac('sha256', KEY);       //设置加密方式，及key值；
-    hmac.update(data);      //对值进行加密
-    const cryData = hmac.digest('hex')   //返回加密后的16进制；
-    return cryData;
+	const crypro = require('crypto');
+	const KEY = 'hejiancheng hen li hai'
+	const hmac = crypro.createHmac('sha256', KEY);       //设置加密方式，及key值；
+	hmac.update(data);      //对值进行加密
+	const cryData = hmac.digest('hex')   //返回加密后的16进制；
+	return cryData;
 }
 /**
  * 判断输入值是否存在
  */
-myUntils.comparison = function (reqData,callback) {
-    _docData = ['users'];
-    _docLen = _docData.length;
-    connectMongo(function () {
-		_checkData.call(null,reqData)
-	})
+let isSta = true;
+myUntils.comparison = function (reqData, callback) {
+	if (isSta) {
+		isSta = false;
+		_docData = ['users'];
+		_docLen = _docData.length;
+		connectMongo(function () {
+			let statu = _checkData.call(null, reqData)
+			isSta = true;
+			callback && callback(statu);
+		})
+	}
+
 }
 
 /**
@@ -68,25 +75,21 @@ myUntils.comparison = function (reqData,callback) {
  * @private
  */
 function _checkData(_reqData) {
-    let _name = _reqData.name,
-        _val = _reqData.val,
-        _staU = false,      //用于判断user是否存在
-        _staP = false,      //用于判断_stap是否正确；
-        state = false;
-    finishPack();
-    let newData = JSON.parse(_mongoData);
-    _ergloop(newData, function (data) {
-        let _newData = newData[data];
-        _ergloop(_newData,function (_data,index) {
-            let checkData = _newData[_data];
-            console.log(checkData[_name],_val)
-            if(checkData[_name] === _val){
-
-            }else {
-
-            }
-        })
-    })
+	let _name = _reqData.name,
+		_val = _reqData.val,
+		state = '可以注册';          //用于判断用户名是否已经被注册
+	finishPack();
+	let newData = JSON.parse(_mongoData);
+	_ergloop(newData, function (data) {
+		let _newData = newData[data];
+		_ergloop(_newData, function (_data) {
+			let checkData = _newData[_data];
+			if (checkData[_name] === _val) {
+				state = '用户名已被注册';
+			}
+		})
+	})
+	return state;
 }
 
 
@@ -94,77 +97,78 @@ function _checkData(_reqData) {
  * 链接数据库；
  */
 function connectMongo(callback) {
-    const MongoClient = require('mongodb').MongoClient;
-    var mongoUrl = 'mongodb://localhost:27017/';
-    MongoClient.connect(mongoUrl, function (err, db) {				//连接数据库
-        if (err) console.log(err);
-        var dbo = db.db('user');									//创建可操作的user对象；
-        // dbo.collection(_docData[0], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
-        //     let artKeyArr = Object.keys(data[0]);
-        //     let _len = artKeyArr.length;
-        //     for (let i = 0; i < _len; i++) {
-        //         _artData[i] = '\"' + artKeyArr[i] + '\":\"' + data[0][artKeyArr[i]] + '\"';
-        //     }
-        //     _artData = _artData.join(',');
-        //     _artData = ',\"article\":{' + _artData + '}';
-        //     _mongoData += _artData;
-        //     eventsEmitter.emit('finishOne')
-        // })
-        // dbo.collection(_docData[1], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
-        //    let conData = [];                                               //创建一个用于存放数据的数组
-        //    	let _mongodata = [];
-        //    	for (let k = 0, kLen = data.length; k < kLen; k++) {
-        //    		let _rKeyArr = Object.keys(data[k]);              //获取每个文档中的key的集合；是一个数组集合
-        //    		let _len = _rKeyArr.length;
-        //    		for (let i = 0; i < _len; i++) {                    //遍历每个文档集合；把集合转成字符串
-        //    			conData[i] = '\"' + _rKeyArr[i] + '\":\"' + data[k][_rKeyArr[i]] + '\"';
-        //    		}
-        //    		_mongodata[k] = conData.join(',');
-        //    		_mongodata[k] = '\"' + mongoName + k + '\":{' + _mongodata[k] + '}';
-        //    	}
-        //    	_mongodata = _mongodata.join(',');
-        //    	_userData = ',\"' + mongoName + '\":{' + _mongodata + '}';
-        //     _mongoData += _userData;
-        //     eventsEmitter.emit('finishOne')
-        // })
-        //
-        // dbo.collection(_docData[2], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
-        //     for (let k = 0, kLen = data.length; k < kLen; k++) {
-        //         let commentKeyArr = Object.keys(data[k]);
-        //         let _len = commentKeyArr.length;
-        //         for (let i = 0; i < _len; i++) {
-        //             _commentData[i] = '\"' + commentKeyArr[i] + '\":\"' + data[k][commentKeyArr[i]] + '\"';
-        //         }
-        //     }
-        //     _commentData = _commentData.join(',');
-        //     _commentData = ','+_docData[2]+':{' + _commentData + '}';
-        //     _mongoData += _commentData;
-        //     eventsEmitter.emit('finishOne')
-        // })
-        for (let _doc = 0; _doc < _docLen; _doc++) {
-            dbo.collection(_docData[_doc], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
-                getData(data, _docData[_doc]);          //data是一个数组对象，其内部存放的格式是，一个文档为一个数组位；
-                callback && callback();
-            })
-        }
+	_mongoData = '';
+	const MongoClient = require('mongodb').MongoClient;
+	var mongoUrl = 'mongodb://localhost:27017/';
+	MongoClient.connect(mongoUrl, function (err, db) {				//连接数据库
+		if (err) console.log(err);
+		var dbo = db.db('user');									//创建可操作的user对象；
+		// dbo.collection(_docData[0], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
+		//     let artKeyArr = Object.keys(data[0]);
+		//     let _len = artKeyArr.length;
+		//     for (let i = 0; i < _len; i++) {
+		//         _artData[i] = '\"' + artKeyArr[i] + '\":\"' + data[0][artKeyArr[i]] + '\"';
+		//     }
+		//     _artData = _artData.join(',');
+		//     _artData = ',\"article\":{' + _artData + '}';
+		//     _mongoData += _artData;
+		//     eventsEmitter.emit('finishOne')
+		// })
+		// dbo.collection(_docData[1], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
+		//    let conData = [];                                               //创建一个用于存放数据的数组
+		//    	let _mongodata = [];
+		//    	for (let k = 0, kLen = data.length; k < kLen; k++) {
+		//    		let _rKeyArr = Object.keys(data[k]);              //获取每个文档中的key的集合；是一个数组集合
+		//    		let _len = _rKeyArr.length;
+		//    		for (let i = 0; i < _len; i++) {                    //遍历每个文档集合；把集合转成字符串
+		//    			conData[i] = '\"' + _rKeyArr[i] + '\":\"' + data[k][_rKeyArr[i]] + '\"';
+		//    		}
+		//    		_mongodata[k] = conData.join(',');
+		//    		_mongodata[k] = '\"' + mongoName + k + '\":{' + _mongodata[k] + '}';
+		//    	}
+		//    	_mongodata = _mongodata.join(',');
+		//    	_userData = ',\"' + mongoName + '\":{' + _mongodata + '}';
+		//     _mongoData += _userData;
+		//     eventsEmitter.emit('finishOne')
+		// })
+		//
+		// dbo.collection(_docData[2], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
+		//     for (let k = 0, kLen = data.length; k < kLen; k++) {
+		//         let commentKeyArr = Object.keys(data[k]);
+		//         let _len = commentKeyArr.length;
+		//         for (let i = 0; i < _len; i++) {
+		//             _commentData[i] = '\"' + commentKeyArr[i] + '\":\"' + data[k][commentKeyArr[i]] + '\"';
+		//         }
+		//     }
+		//     _commentData = _commentData.join(',');
+		//     _commentData = ','+_docData[2]+':{' + _commentData + '}';
+		//     _mongoData += _commentData;
+		//     eventsEmitter.emit('finishOne')
+		// })
+		for (let _doc = 0; _doc < _docLen; _doc++) {
+			dbo.collection(_docData[_doc], {useNewUrlParser: true}).find({}).toArray(function (err, data) {
+				getData(data, _docData[_doc]);          //data是一个数组对象，其内部存放的格式是，一个文档为一个数组位；
+				callback && callback();
+			})
+		}
 
-        function getData(data, mongoName) {                                             //如果data 是一个json对象，
-            let conData = [];                                               //创建一个用于存放数据的数组
-            let _mongodata = [];
-            for (let k = 0, kLen = data.length; k < kLen; k++) {
-                let _rKeyArr = Object.keys(data[k]);              //获取每个文档中的key的集合；是一个数组集合
-                let _len = _rKeyArr.length;
-                for (let i = 0; i < _len; i++) {                    //遍历每个文档集合；把集合转成字符串
-                    conData[i] = '\"' + _rKeyArr[i] + '\":\"' + data[k][_rKeyArr[i]] + '\"';
-                }
-                _mongodata[k] = conData.join(',');
-                _mongodata[k] = '\"' + mongoName + k + '\":{' + _mongodata[k] + '}';
-            }
-            _mongodata = _mongodata.join(',');
-            _mongodata = ',\"' + mongoName + '\":{' + _mongodata + '}';
-            _mongoData += _mongodata;
-        }
-    });
+		function getData(data, mongoName) {                                             //如果data 是一个json对象，
+			let conData = [];                                               //创建一个用于存放数据的数组
+			let _mongodata = [];
+			for (let k = 0, kLen = data.length; k < kLen; k++) {
+				let _rKeyArr = Object.keys(data[k]);              //获取每个文档中的key的集合；是一个数组集合
+				let _len = _rKeyArr.length;
+				for (let i = 0; i < _len; i++) {                    //遍历每个文档集合；把集合转成字符串
+					conData[i] = '\"' + _rKeyArr[i] + '\":\"' + data[k][_rKeyArr[i]] + '\"';
+				}
+				_mongodata[k] = conData.join(',');
+				_mongodata[k] = '\"' + mongoName + k + '\":{' + _mongodata[k] + '}';
+			}
+			_mongodata = _mongodata.join(',');
+			_mongodata = ',\"' + mongoName + '\":{' + _mongodata + '}';
+			_mongoData += _mongodata;
+		}
+	});
 
 }
 
@@ -172,8 +176,8 @@ function connectMongo(callback) {
  * 处理获取的最后的数据
  */
 function finishPack() {
-    _mongoData = _mongoData.substr(1)
-    _mongoData = '{' + _mongoData + '}';
+	_mongoData = _mongoData.substr(1)
+	_mongoData = '{' + _mongoData + '}';
 }
 
 /**
@@ -184,41 +188,41 @@ function finishPack() {
  * @private
  */
 function _choiceData(choice, data) {
-    let _data = JSON.parse(data);
-    // console.log(typeof _data);
-    let resData = {};
-    let userData = _data.users;
-    let redDataItem = [];
-    if (choice) {
-        _ergloop(choice, function (key) {
-            // console.log(key)
-            const _dataKey = _ergloop(_data);
-            let _dataIndex = _dataKey.indexOf(key);
-            // console.log(_dataKey[_dataIndex] )          //获取文档名；
-            // if (_dataKey[_dataIndex] === 'users') {
-            //     //如果是要获取users文档时，需要根据art的数据获取相关user的对象；因为只用获取一个文档；
-            //     let userItem = _data[_dataKey[_dataIndex]];
-            //     _ergloop(userItem, function (userKey) {
-            //         if (userItem[userKey]._id === resData.articals.auther) {
-            //             let _dataValue = _data[_dataKey[_dataIndex]]            //获取user文档下的的文档对象；
-            //             let choiceData = choice[key].split(',');
-            //             resData[_dataKey[_dataIndex]] = _getData(_dataValue, choiceData);
-            //         }
-            //     })
-            // } else {
-            let _dataValue = _data[_dataKey[_dataIndex]]            //获取artical或者user文档下的的文档对象；
-            // console.log(_dataValue)
-            let choiceData = choice[key].split(',');
-            let _dataItems = _data[_dataKey[_dataIndex]];
-            _ergloop(_dataItems, function (itemKey, index) {
-                // console.log(index);
-                redDataItem[index] = _getData(_dataValue, choiceData, userData);
-            })
-            resData[_dataKey[_dataIndex]] = redDataItem;
-        });
-    }
-    // console.log(JSON.stringify(resData))
-    return JSON.stringify(resData);
+	let _data = JSON.parse(data);
+	// console.log(typeof _data);
+	let resData = {};
+	let userData = _data.users;
+	let redDataItem = [];
+	if (choice) {
+		_ergloop(choice, function (key) {
+			// console.log(key)
+			const _dataKey = _ergloop(_data);
+			let _dataIndex = _dataKey.indexOf(key);
+			// console.log(_dataKey[_dataIndex] )          //获取文档名；
+			// if (_dataKey[_dataIndex] === 'users') {
+			//     //如果是要获取users文档时，需要根据art的数据获取相关user的对象；因为只用获取一个文档；
+			//     let userItem = _data[_dataKey[_dataIndex]];
+			//     _ergloop(userItem, function (userKey) {
+			//         if (userItem[userKey]._id === resData.articals.auther) {
+			//             let _dataValue = _data[_dataKey[_dataIndex]]            //获取user文档下的的文档对象；
+			//             let choiceData = choice[key].split(',');
+			//             resData[_dataKey[_dataIndex]] = _getData(_dataValue, choiceData);
+			//         }
+			//     })
+			// } else {
+			let _dataValue = _data[_dataKey[_dataIndex]]            //获取artical或者user文档下的的文档对象；
+			// console.log(_dataValue)
+			let choiceData = choice[key].split(',');
+			let _dataItems = _data[_dataKey[_dataIndex]];
+			_ergloop(_dataItems, function (itemKey, index) {
+				// console.log(index);
+				redDataItem[index] = _getData(_dataValue, choiceData, userData);
+			})
+			resData[_dataKey[_dataIndex]] = redDataItem;
+		});
+	}
+	// console.log(JSON.stringify(resData))
+	return JSON.stringify(resData);
 }
 
 /**
@@ -228,27 +232,27 @@ function _choiceData(choice, data) {
  * @private
  */
 function _getData(dataValue, regChoice, userData) {
-    let resdata = {};
-    _ergloop(dataValue, function (dataKey) {                                                     //遍历所有文档，获取其值；
-        _ergloop(regChoice, (choiceKey) => {                                                   //遍历选择的规则，即choice中的值；
-            if (regChoice[choiceKey] == 'auther') {                  //如果遍历到了auther时，就把对应的user对象给他；
-                _ergloop(userData, function (key, index) {                                //遍历所有用户文档
-                    _ergloop(userData[key], function (userItem) {                          //遍历所有用户对象
-                        if (userData[key][userItem] === dataValue[dataKey].auther) {      //寻找与当前文章的作者匹配的用户对象并赋值
-                            resdata[regChoice[choiceKey]] = userData[key];
-                        }
-                    })
-                })
-            } else if (regChoice[choiceKey] == 'created') {
-                let date = dataValue[dataKey][regChoice[choiceKey]];
-                let artdate = new Date().dateForm(new Date(date));
-                resdata["created"] = artdate;           //获取其最终值；并保存到resdata中；
-            } else {
-                resdata[regChoice[choiceKey]] = dataValue[dataKey][regChoice[choiceKey]];           //获取其最终值；并保存到resdata中；
-            }
-        })
-    })
-    return resdata;
+	let resdata = {};
+	_ergloop(dataValue, function (dataKey) {                                                     //遍历所有文档，获取其值；
+		_ergloop(regChoice, (choiceKey) => {                                                   //遍历选择的规则，即choice中的值；
+			if (regChoice[choiceKey] == 'auther') {                  //如果遍历到了auther时，就把对应的user对象给他；
+				_ergloop(userData, function (key, index) {                                //遍历所有用户文档
+					_ergloop(userData[key], function (userItem) {                          //遍历所有用户对象
+						if (userData[key][userItem] === dataValue[dataKey].auther) {      //寻找与当前文章的作者匹配的用户对象并赋值
+							resdata[regChoice[choiceKey]] = userData[key];
+						}
+					})
+				})
+			} else if (regChoice[choiceKey] == 'created') {
+				let date = dataValue[dataKey][regChoice[choiceKey]];
+				let artdate = new Date().dateForm(new Date(date));
+				resdata["created"] = artdate;           //获取其最终值；并保存到resdata中；
+			} else {
+				resdata[regChoice[choiceKey]] = dataValue[dataKey][regChoice[choiceKey]];           //获取其最终值；并保存到resdata中；
+			}
+		})
+	})
+	return resdata;
 }
 
 /**
@@ -257,34 +261,21 @@ function _getData(dataValue, regChoice, userData) {
  * @private
  */
 function _ergloop(ergdata, callback) {
-    if (typeof ergdata == 'object') {
-        let _ergdata = Object.keys(ergdata),
-            _erglen = _ergdata.length;
-        for (let i = 0; i < _erglen; i++) {
-            // console.log(i)
-            callback && callback(_ergdata[i], i);
-        }
-        return _ergdata;
-    }
+	if (typeof ergdata == 'object') {
+		let _ergdata = Object.keys(ergdata),
+			_erglen = _ergdata.length;
+		for (let i = 0; i < _erglen; i++) {
+			callback && callback(_ergdata[i], i);
+		}
+		return _ergdata;
+	}
 }
-
-//
-// /**
-//  * 循环遍历函数
-//  * @param data  需要遍历的对象
-//  */
-// function _ergLoop(data) {
-//     for (let keys in data) {
-//         let _value = data[keys];
-//         createArtEle(_value);
-//     }
-// }
 
 
 Date.prototype.dateForm = function (date) {
-    let _dateObj = [date.getFullYear(), '年', date.getMonth() + 1,
-        '月', date.getDate(), '日']
-    return _dateObj.join('');
+	let _dateObj = [date.getFullYear(), '年', date.getMonth() + 1,
+		'月', date.getDate(), '日']
+	return _dateObj.join('');
 }
 
 
@@ -292,3 +283,4 @@ myUntils.event = event;
 module.exports = myUntils;
 
 //考虑到获取的评论数据内容不是固定的数量
+
--- view/js/login.js
+++ view/js/login.js
@@ -1,26 +1,45 @@
 const url = window.location.href;
 window.onload = function () {
-    let inpU = document.getElementsByClassName('username')[0],
-        inpP = document.getElementsByClassName('password')[0],
-        proU = document.getElementsByClassName('pro-use')[0],
-        proP = document.getElementsByClassName('pro-pass')[0],
-        _tips = document.getElementsByClassName('tips')[0],
-        btnSub = document.getElementsByClassName('pro-sub')[0],
-        btnRes = document.getElementsByClassName('pro-res')[0];
-    inpU.onblur = function (){addTips.call(this)};
-    inpP.onblur = function (){addTips.call(this)};
+	let inpU = document.getElementsByClassName('username')[0],
+		inpP = document.getElementsByClassName('password')[0],
+		inpRep = document.getElementsByClassName('repassword')[0],
+		proU = document.getElementsByClassName('pro-use')[0],
+		proP = document.getElementsByClassName('pro-pass')[0],
+		_tips = document.getElementsByClassName('tips'),
+		btnSub = document.getElementsByClassName('pro-sub')[0],
+		btnRes = document.getElementsByClassName('pro-res')[0];
+	inpU.onblur = function () {
+		addTips.call(this)
+	};
+	inpP.onblur = function () {
+		addTips.call(this)
+	};
+	if (inpRep) {
+		inpRep.onblur = function () {
+			if (inpP.value !== inpRep.value) {
+				this.style.borderColor = 'red';
+				_tips[2].innerHTML = '两次输入的密码不一致';
+				_tips[2].style.color = 'red';
+			} else {
+				this.style.borderColor = 'rgb(238,238,238)';
+				this.style.borderStyle = 'groove';
+				_tips[2].innerHTML = '两次密码一致';
+				_tips[2].style.color = 'rgba(1,1,1,.2)';
+			}
+		};
+	}
 
-    function addTips(){
-        let _val = this.value;
-        let _key = this.name;
-        ajax({
-            type:'POST',
-            url:url,
-            data:{name:_key,val:_val},
-            success:function (data) {
-				_tips.innerHTML = data;
-            }
-        })
-    }
+	function addTips() {
+		let _val = this.value;
+		let _key = this.name;
+		ajax({
+			type: 'POST',
+			url: url,
+			data: {name: _key, val: _val},
+			success: function (data) {
+				_tips[0].innerHTML = data;
+			}
+		})
+	}
 
 }
